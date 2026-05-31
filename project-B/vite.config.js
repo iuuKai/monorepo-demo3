10aa7c915b2ddf79168e83a1fb98e1fb027b12ea
iuukai <790331286@163.com>
2026-05-31 09:30:03 +0800

    rb111

--- project-B/vite.config.js
+++ project-B/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [vue()],
-	base: '/project-B/'
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+	const baseURL = env.BASE_URL
+
+	return {
+		base: baseURL,
+		plugins: [vue()]
+	}
 })
