a50d2dc2ba9813a01a44e479ca585a2cac60d298
iuukai <790331286@163.com>
2026-06-06 01:44:00 +0800

    f9

--- project-F/docs/.vuepress/config.js
+++ project-F/docs/.vuepress/config.js
@@ -1,32 +1,39 @@
+import 'dotenv/config'
 import { defineUserConfig } from 'vuepress'
-import { viteBundler } from '@vuepress/bundler-vite' // 启用Vite打包
+import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 
+const isBuild = process.env.NODE_ENV === 'production'
+if (isBuild && !process.env.BASE_URL) {
+	throw new Error('BASE_URL is required in production mode')
+}
+const BASE_URL = isBuild ? process.env.BASE_URL : '/'
+
 export default defineUserConfig({
-	base: process.env.BASE_URL,
-	// 指定使用Vite作为打包工具（关键）
+	base: BASE_URL,
 	bundler: viteBundler({
-		// 自定义Vite原生配置，直接透传给Vite
 		viteOptions: {
-			// 原生vite.config配置，别名、插件、服务端口都可写这里
 			server: { port: 8088 },
 			resolve: {
 				alias: { '@': './docs/.vuepress' }
 			}
 		}
 	}),
-	// 站点基础配置
 	title: 'VuePress',
 	description: 'VuePress文档',
 	theme: defaultTheme({
-		// 导航栏配置
 		navbar: [
 			{ text: '首页', link: '/' },
-			{ text: '指南', link: '/guide/' }
+			{ text: '关于', link: '/about/' }
 		],
-		// 侧边栏
 		sidebar: {
-			'/guide/': ['intro']
+			'/about/': [
+				{
+					title: '关于',
+					collapsable: false,
+					children: ['', 'page1']
+				}
+			]
 		}
 	})
 })
--- project-F/docs/README.md
+++ project-F/docs/README.md
@@ -1,10 +1,24 @@
 ---
 home: true
-heroText: VuePress文档站
-tagline: VuePress2
-actionText: 快速上手 →
-actionLink: /guide/
+heroText: VuePress
+tagline: Home
+actions:
+  - text: 首页
+    link: /
+    type: primary
+  - text: 关于
+    link: /about/
+    type: default
+  - text: 404
+    link: /404/
+    type: default
 footer: Powered by VuePress
 ---
 
-ddd
+<img src="/images/demo.jpg" alt="demo" width="200" height="200">
+
+<style scoped>
+img {
+ object-fit: cover;
+}
+</style>
